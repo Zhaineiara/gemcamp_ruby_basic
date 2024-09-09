5332832e3571e6193a1d3f913f5e228bc88b41f7
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-09 17:01:35 +0800

    Question_19: Product of inputted values.

---
+++ printing_variables_operators/question_19.rb
@@ -0,0 +1,9 @@
+print "Enter the first number: "
+first_number = gets.chomp.to_i
+
+print "Enter the second number: "
+second_number = gets.chomp.to_i
+
+product = first_number * second_number
+
+print "Product: #{product}"
