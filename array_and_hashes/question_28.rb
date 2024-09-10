126e5c381c3a6e68c8080532401d073c72d114cb
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-10 18:04:25 +0800

    Question_28: Movie information in a hash.

---
+++ array_and_hashes/question_28.rb
@@ -0,0 +1,22 @@
+anime_movies = {
+  "Spirited Away" => {
+    title: "Spirited Away",
+    director: "Hayao Miyazaki",
+    year: 2001,
+    rating: 8.6
+  },
+  "My Neighbor Totoro" => {
+    title: "My Neighbor Totoro",
+    director: "Hayao Miyazaki",
+    year: 1988,
+    rating: 8.1
+  },
+  "Akira" => {
+    title: "Akira",
+    director: "Katsuhiro Otomo",
+    year: 1988,
+    rating: 8.0
+  }
+}
+
+print anime_movies
