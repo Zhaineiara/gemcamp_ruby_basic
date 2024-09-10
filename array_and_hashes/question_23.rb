11302c40b4c77e9412e239ef64f3a24df60b8568
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-10 17:45:19 +0800

    Question_23: Author of the second book.

---
+++ array_and_hashes/question_23.rb
@@ -0,0 +1,7 @@
+anime_books = {
+  "Akira" => "Katsuhiro Otomo",
+  "Naruto: The Official Character Data Book" => "Masashi Kishimoto",
+  "Fullmetal Alchemist: The Complete Guide" => "Hiromu Arakawa"
+}
+
+print anime_books["Naruto: The Official Character Data Book"]
