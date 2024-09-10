581576e642bdc18699ecd2fcd4da233c0833a82f
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-10 15:20:38 +0800

    Question_5: Ascending order of numbers.

--- array_and_hashes/question_5.rb
+++ array_and_hashes/question_5.rb
@@ -1,4 +1,4 @@
-array_number = [2, 5, 3, 8, 1]
-ascending_number = array_number.sort
+array_numbers = [2, 5, 3, 8, 1]
+ascending_numbers = array_number.sort
 
-print ascending_number
+print ascending_numbers
