e0d45f09260c281877ea3d36c5b1f3e29566d94f
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-10 19:03:45 +0800

    Question_37: Name of 10th student.

---
+++ array_and_hashes/question_37.rb
@@ -0,0 +1,54 @@
+students = [
+  {
+    "name" => "Alice",
+    "age" => 20,
+    "grade" => "A"
+  },
+  {
+    "name" => "Bob",
+    "age" => 22,
+    "grade" => "B"
+  },
+  {
+    "name" => "Charlie",
+    "age" => 23,
+    "grade" => "A"
+  },
+  {
+    "name" => "Diana",
+    "age" => 21,
+    "grade" => "C"
+  },
+  {
+    "name" => "Edward",
+    "age" => 24,
+    "grade" => "B"
+  },
+  {
+    "name" => "Fiona",
+    "age" => 19,
+    "grade" => "A"
+  },
+  {
+    "name" => "George",
+    "age" => 22,
+    "grade" => "C"
+  },
+  {
+    "name" => "Hannah",
+    "age" => 21,
+    "grade" => "B"
+  },
+  {
+    "name" => "Ian",
+    "age" => 23,
+    "grade" => "A"
+  },
+  {
+    "name" => "Jane",
+    "age" => 20,
+    "grade" => "B"
+  }
+]
+
+print students[9]["name"]
