55a184d9d4e4ae8bcd9bc5c37b3fc3d162e8e3e8
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-10 15:29:30 +0800

    Question_7: Determine array length.

---
+++ array_and_hashes/question_7.rb
@@ -0,0 +1,11 @@
+anime_movies = [
+  "Spirited Away (2001)",
+  "My Neighbor Totoro (1988)",
+  "Akira (1988)",
+  "Your Name (2016)",
+  "Princess Mononoke (1997)",
+]
+
+array_length = anime_movies.length
+
+print "Array length: #{array_length}"
