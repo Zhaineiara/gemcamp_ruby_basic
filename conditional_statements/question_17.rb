c4f96b68d75d6b1fe1db1d48affbcb468d671e71
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-11 15:59:31 +0800

    Question_17: Identical word or different word.

---
+++ conditional_statements/question_17.rb
@@ -0,0 +1,11 @@
+print "Enter first word: "
+given_word_1 = gets.chomp
+
+print "Enter second word: "
+given_word_2 = gets.chomp
+
+if given_word_1 == given_word_2
+  print "The words are identical"
+else
+  print "The words are different"
+end
