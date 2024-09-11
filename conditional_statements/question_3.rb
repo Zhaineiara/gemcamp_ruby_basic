61a224a27ada32b15acf8f3057e887a127b4a414
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-11 15:17:50 +0800

    Question_3: Checking equal or not.

---
+++ conditional_statements/question_3.rb
@@ -0,0 +1,11 @@
+print "Enter first number: "
+given_number_1 = gets.chomp.to_i
+print "Enter second number: "
+given_number_2 = gets.chomp.to_i
+
+if given_number_1 ==given_number_2
+  print "The numbers are equal"
+else
+  print"The numbers are not equal"
+end
+
