7e78a7a9bf05af7bedf888d29a8bcc52668af047
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-11 18:13:55 +0800

    Question_34: Positive numbers in array.

--- conditional_statements/question_34.rb
+++ conditional_statements/question_34.rb
@@ -1,8 +1,10 @@
 numbers = [-12, -2, 7, -10, 4, 0, -15, 14, 8, -4, 13, -3, 12, 9, 5]
+positive_numbers = []
 
 numbers.each do |number|
   if number > 0
-    puts number
+    positive_numbers.push(number)
   end
 end
 
+p positive_numbers.length
