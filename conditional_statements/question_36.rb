a8a45525d3f00fc0d254351beb7376ffbac03b9c
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-11 18:41:20 +0800

    Question_36: Change to capitalized names.

---
+++ conditional_statements/question_36.rb
@@ -0,0 +1,7 @@
+names = ["alice", "BOB", "eve", "DAN", "CHRIS", "max", "sue", "ANNA", "liam", "joe", "ZOE", "paul", "ian", "MIA", "ted"]
+
+names.each do |name|
+  name.capitalize!
+end
+
+print names
