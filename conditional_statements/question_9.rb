8e1cc1d0e661abfa1e4a562cfa5cc94f948ec508
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-11 15:41:36 +0800

    Question_9: Check emptiness.

---
+++ conditional_statements/question_9.rb
@@ -0,0 +1,10 @@
+print "Enter a text: "
+given_text = gets.chomp
+
+emptiness_check = given_text.empty?
+
+if emptiness_check == true
+  print "You entered an empty string"
+else
+  print "You entered a string"
+end
